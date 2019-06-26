2b1d2ea8f4df32b9a17d97c18df29326f87fad35
Johan Del Valle <43177845+JohanDelValleV@users.noreply.github.com>
2019-06-26 10:00:49 -0500

    .

--- Alumno/urls.py
+++ Alumno/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include
 from django.contrib.auth.models import User
 
 from Alumno import views
-
+ 
 urlpatterns = [
     re_path(r'alumno_lista/$', views.AlumnoList.as_view()),
     re_path(r'alumno_detail/(?P<id>\d+)$', views.AlumnoDetail.as_view()),
