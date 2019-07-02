b77d4ae90012cbf7d6cd415deff4ebca8bf2b5c0
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-07-02 17:43:51 -0500

    ya quedo el get

--- Rfid/models.py
+++ Rfid/models.py
@@ -6,6 +6,7 @@ from Alumno.models import Alumno
 class Rfid(models.Model):
     id_rfid = models.IntegerField(null=False)
     id_alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    delete = models.BooleanField(default=False)
     created = models.DateTimeField(default=timezone.now)
     def _str_(self):
         return self
