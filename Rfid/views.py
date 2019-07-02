2849b0b34b5b7096de5eb9826e3e5c3bd3ae5da8
Johan Del Valle <43177845+JohanDelValleV@users.noreply.github.com>
2019-07-02 15:22:32 -0500

    Datos del alumno

--- Rfid/views.py
+++ Rfid/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.http import Http404
+from django.http import Http404, JsonResponse
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,6 +8,8 @@ from rest_framework import generics
 
 from Rfid.models import Rfid
 from Rfid.serializer import RfidSerializers
+
+
 
 class RfidList(APIView):
     def get(self, request, format=None):
@@ -20,13 +22,13 @@ class RfidList(APIView):
         if serializer.is_valid():
             serializer.save()
             datas = serializer.data
-            return Response(datas)
+            return JsonResponse({'id':datas['id']})
         return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
 
 class RfidDetail(APIView):
     def get_object(self, id):
         try:
-            return Rfid.objects.get(pk=id)
+            return Rfid.objects.get(id_rfid=id)
         except:
             return 404
 
