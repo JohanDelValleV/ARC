7a5c5800b4f9c98e0fd4531e3f9b7738d45eb50c
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-07-03 08:42:46 -0500

    Update serializer.py

--- Asistencia/serializer.py
+++ Asistencia/serializer.py
@@ -3,6 +3,8 @@ from rest_framework import routers, serializers, viewsets
 from Asistencia.models import Asistencia
 
 class AsistenciaSerializers(serializers.ModelSerializer):
+    _nombre = serializers.ReadOnlyField(source='rfid.id_alumno.name')
+    _lastname = serializers.ReadOnlyField(source='rfid.id_alumno.lastname')
     class Meta:
         model = Asistencia
         fields = ('__all__')
